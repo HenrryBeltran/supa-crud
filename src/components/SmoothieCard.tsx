71bda876e9246c8630c23d6c4fa3a747acf6847f
Henrry Beltrán <henrry26bt@gmail.com>
2023-05-19 21:51:27 -0500

    All supabase database manipulation completed

--- src/components/SmoothieCard.tsx
+++ src/components/SmoothieCard.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import RatingCard from "./RatingCard";
 import moment from "moment";
+
+import RatingCard from "./RatingCard";
+import supabase from "../supabase/client";
 
 type Smoothie = {
   smoothie: {
@@ -10,9 +12,25 @@ type Smoothie = {
     method: string;
     rating: number;
   };
+  onDelete: (id: number) => void;
 };
 
-const SmoothieCard: React.FC<Smoothie> = ({ smoothie }) => {
+const SmoothieCard: React.FC<Smoothie> = ({ smoothie, onDelete }) => {
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      .from("Smoothies")
+      .delete()
+      .eq("id", smoothie.id)
+      .select();
+
+    if (error) {
+      console.log(error);
+    }
+    if (data) {
+      onDelete(smoothie.id);
+    }
+  };
+
   return (
     <div className="grid grid-cols-2 grid-rows-2 rounded-md bg-sky-50 p-4 shadow-md lg:p-6">
       <div className="col-span-2 mb-3">
@@ -42,8 +60,8 @@ const SmoothieCard: React.FC<Smoothie> = ({ smoothie }) => {
             <path d="M180 876h44l443-443-44-44-443 443v44Zm614-486L666 262l42-42q17-17 42-17t42 17l44 44q17 17 17 42t-17 42l-42 42Zm-42 42L248 936H120V808l504-504 128 128Zm-107-21-22-22 44 44-22-22Z" />
           </svg>
         </a>
-        <a
-          href={`/edit/${smoothie.id}`}
+        <button
+          onClick={handleDelete}
           className="rounded-full p-3 hover:bg-sky-200/50 lg:p-2"
         >
           <svg
@@ -52,7 +70,7 @@ const SmoothieCard: React.FC<Smoothie> = ({ smoothie }) => {
           >
             <path d="M261 936q-24.75 0-42.375-17.625T201 876V306h-41v-60h188v-30h264v30h188v60h-41v570q0 24-18 42t-42 18H261Zm438-630H261v570h438V306ZM367 790h60V391h-60v399Zm166 0h60V391h-60v399ZM261 306v570-570Z" />
           </svg>
-        </a>
+        </button>
       </div>
     </div>
   );
---
+++ src/components/SmoothiesContent.tsx
@@ -0,0 +1,149 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+import supabase from "../supabase/client";
+import SmoothieCard from "./SmoothieCard";
+
+interface Smoothie {
+  id: number;
+  created_at: string;
+  title: string;
+  method: string;
+  rating: number;
+}
+
+const filters = [
+  {
+    id: 1,
+    name: "time",
+    type: "created_at",
+    isAscending: false,
+  },
+  {
+    id: 2,
+    name: "name",
+    type: "title",
+    isAscending: true,
+  },
+  {
+    id: 3,
+    name: "rating",
+    type: "rating",
+    isAscending: false,
+  },
+];
+
+const SmoothiesContent = () => {
+  const [fetchError, setFetchError] = useState<string | null>(null);
+  const [smoothies, setSmoothies] = useState<Smoothie[] | null>(null);
+  const [orderBy, setOrderBy] = useState(filters[0]);
+
+  const handleDelete = (id: number) => {
+    setSmoothies((prevSmoothies) => {
+      if (prevSmoothies) {
+        return prevSmoothies.filter((sm) => sm.id !== id);
+      } else {
+        return null;
+      }
+    });
+  };
+
+  useEffect(() => {
+    const fetchSmoothies = async () => {
+      const { data, error } = await supabase
+        .from("Smoothies")
+        .select<"*", Smoothie>("*")
+        .order(orderBy.type, { ascending: orderBy.isAscending });
+
+      if (error) {
+        setFetchError("Could not fetch the smoothies");
+        setSmoothies(null);
+        console.log(error);
+      }
+      if (data) {
+        setSmoothies(data);
+        setFetchError(null);
+      }
+    };
+
+    fetchSmoothies();
+  }, [orderBy]);
+
+  return (
+    <section className="pt-6 lg:px-32 lg:pt-12">
+      <div className="mb-8 space-y-2">
+        <p className="pl-2 text-sm text-sky-700">Order by</p>
+        <Listbox value={orderBy} onChange={setOrderBy}>
+          <div className="relative text-sky-900">
+            <Listbox.Button className="relative cursor-default rounded-lg bg-sky-50 py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-sky-300 sm:text-sm">
+              <span className="block truncate">{orderBy.name}</span>
+              <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                <ChevronUpDownIcon
+                  className="h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+              </span>
+            </Listbox.Button>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Listbox.Options className="absolute mt-1 max-h-60 w-32 overflow-auto rounded-md bg-sky-50 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {filters.map((order, index) => (
+                  <Listbox.Option
+                    key={index}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? "bg-sky-100 text-sky-900" : "text-sky-900"
+                      }`
+                    }
+                    value={order}
+                  >
+                    {({ selected }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {order.name}
+                        </span>
+                        {selected ? (
+                          <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-sky-600">
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
+          </div>
+        </Listbox>
+      </div>
+
+      {fetchError && (
+        <p className="text-center text-xl font-bold text-rose-500">
+          {fetchError}
+        </p>
+      )}
+      {smoothies && (
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:gap-5 xl:grid-cols-4">
+          {smoothies?.map((smoothie) => (
+            <SmoothieCard
+              key={smoothie.id}
+              smoothie={smoothie}
+              onDelete={handleDelete}
+            />
+          ))}
+        </div>
+      )}
+    </section>
+  );
+};
+
+export default SmoothiesContent;
